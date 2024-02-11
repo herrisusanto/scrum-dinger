be8973f145c000f82b9d31483da24841df16cfdc
Heri Susanto <herisusanto.app@gmail.com>
2024-02-11 22:05:18 +0700

    Add Detail Edit View

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetilEditView.swift
+//  Scrumdinger
+//
+//  Created by loratech on 11/02/24.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
