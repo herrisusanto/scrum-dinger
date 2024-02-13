245defc6439705ed637f63dc53daadb7c8d50ca3
Heri Susanto <herisusanto.app@gmail.com>
2024-02-14 05:57:22 +0700

    Add Error Wrapper Add Error View

---
+++ Scrumdinger/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by loratech on 14/02/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by loratech on 14/02/24.
+//
+
+import Foundation
